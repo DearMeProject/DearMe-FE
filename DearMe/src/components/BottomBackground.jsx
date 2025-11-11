a64e0c4859149b165171a8c83b1cee4cc92b38bb
JuHyeong <blane7777@naver.com>
2025-11-11 23:04:30 +0900

    feat: 하단 배경 UI 추가

---
+++ DearMe/src/components/BottomBackground.jsx
@@ -0,0 +1,12 @@
+import '../styles/BottomBackground.css';
+import '../styles/WelcomeSections.css';
+
+function BottomBackground() {
+    return (
+        <div className='bottom-background-container'>
+
+        </div>
+    )
+}
+
+export default BottomBackground;
