fcea18d3ac9c2a9397a790d9a1d809f5c28a6ff1
wnwngud <blane777@naver.com>
2025-11-11 16:07:31 +0900

    feat: 상단 헤더 바 UI 추가

--- DearMe/src/App.jsx
+++ DearMe/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react';
 import useTimePeriod from './hooks/useTimePeriod';
 import Background from './components/Background';
+import Header from './components/Header';
 import './styles/App.css';
 
 function App() {
   return (
     <div>
       <Background />
+      <Header/>
     </div>
   )
 }
