a298b01390c33f8008872d87c66d73399404ec5b
JuHyeong <blane7777@naver.com>
2025-11-14 19:14:20 +0900

    feat: 메모 확인 UI 추가

--- DearMe/src/components/MemoCard.jsx
+++ DearMe/src/components/MemoCard.jsx
@@ -1,16 +1,33 @@
 import '../styles/MemoCard.css';
+import { useState } from "react";
+import MemoRead from './MemoRead';
 
 function MemoCard({ memoData }) {
+
+    const [selectedMemo, setSelectedMemo] = useState(null);
+
     return (
         <>
             {
-                memoData.map((memo, index) => 
-                    <div className='memo-card-container'>
+                memoData.map((memo, index) => (
+                    <div 
+                        key={memo.memoId} 
+                        className='memo-card-container'
+                        onClick={() => {
+                            setSelectedMemo(memo);
+                        }}>
                         <p className='memo-card-emoji'>{memo.emoji}</p>
                         <p className='memo-card-title'>{memo.title}</p>
-                    </div>          
-                )
+                    </div>
+                ))
             }
+
+            {selectedMemo && (
+                <MemoRead 
+                    memo={selectedMemo} 
+                    onClose={() => setSelectedMemo(null)} 
+                />
+            )}
         </>
     )
 }
